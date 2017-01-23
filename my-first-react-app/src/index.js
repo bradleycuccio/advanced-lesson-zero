46994ce3478368f42b0a49726363b695e2a82a49
Bradley Cuccio <bradleycuccio@gmail.com>
2017-01-22 18:52:44 -0600

    first pass at hello world v2

--- my-first-react-app/src/index.js
+++ my-first-react-app/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import './index.css';
 
-const App = function() {
-  return (
-    <h1>Hello React!</h1>
-  );
-}
+//const users = [
+//  { _id: 1, name: 'Leia' },
+//  { _id: 2, name: 'Luke' },
+//  { _id: 3, name: 'Han' }
+//]; attempting to pass the users as a props to in app
 
 ReactDOM.render(
   <App />,
